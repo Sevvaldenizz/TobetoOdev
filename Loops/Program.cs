718d8858b175d0a3873f654d7f3fe980c2d0aaff
sevvalkaba0@gmail.com <sevvalkaba0@gmail.com>
2023-10-24 23:37:42 +0300

    C# Dersleri 30: For Döngüsü

---
+++ Loops/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           for (int i = 100; i >= 0; i=i-2) 
+            {
+            Console.WriteLine(i);
+            }
+           Console.WriteLine("Finished!");
+            
+            
+            Console.ReadLine();
+        }
+    }
+}
