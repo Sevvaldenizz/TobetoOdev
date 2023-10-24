1187814195cffe4d06df01c52456c25970580765
sevvalkaba0@gmail.com <sevvalkaba0@gmail.com>
2023-10-24 19:19:17 +0300

    C# Dersleri 16: Single Line If İle Çalışmak

--- Conditionals/Program.cs
+++ Conditionals/Program.cs
@@ -7,14 +7,14 @@
             var number = 11;
             if (number==10)
             {
-                Console.WriteLine("Number is 10");
+                Console.WriteLine("Number is 10"); 
             }
             else
             {
                 Console.WriteLine("Number is not 10");
             }
 
-            Console.ReadLine();
+            Console.ReadLine(); 
         }
     }
 }
