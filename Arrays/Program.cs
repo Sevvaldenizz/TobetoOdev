d176c2fe4a58beff0a1c028183a5bb0cb252c698
sevvalkaba0@gmail.com <sevvalkaba0@gmail.com>
2023-10-24 22:59:01 +0300

    C# Dersleri 28: Diziler

---
+++ Arrays/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] students = new string[3] { "Engin", "Derin", "Salih" };
+            students2[3] = "Ahmet";
+
+            string[] students2 = { "Engin", "Derin", "Salih" }; 
+        
+
+            foreach (var student in students) 
+            {
+                Console.WriteLine(student);            
+            }
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
