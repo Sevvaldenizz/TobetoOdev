1f2106fd80050c0ed4c144dd210d9e1c3bbc2ddf
sevvalkaba0@gmail.com <sevvalkaba0@gmail.com>
2023-10-24 16:36:38 +0300

    C# Dersleri 6: Long Veri Tipi

--- TypesAndVariables/Program.cs
+++ TypesAndVariables/Program.cs
@@ -9,10 +9,11 @@
 
 
             int number1 = 2147483647;
-
+            long number2 = 2147483648;
 
             Console.WriteLine("Number1 is {0}", number1);
-          
+            Console.WriteLine("Number2 is {0}", number2);
+
         }
     }
 }
