f8ee12c1ec2ab7efb1ec2c7859ba4f8b3963492f
sevvalkaba0@gmail.com <sevvalkaba0@gmail.com>
2023-10-24 21:33:51 +0300

    C# Dersleri 24: Challenge : Ref Keyword İle Çalışmak

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -6,12 +6,17 @@ namespace Methods
     {
         static void Main(string[] args)
         {
-            Add();
-            Add();
-            Add();
-            Add();
-            var result = Add2();
-            Console.WriteLine(result);
+            //Add();
+            //Add();
+            //Add();
+            //Add();
+            //var result = Add2();
+
+            int number1 = 20;
+            int number2 = 100;
+            var result2 = Add3(ref number1, number2);
+            Console.WriteLine(result2);
+            Console.WriteLine(number1);
             Console.ReadLine();
 
         }
@@ -26,6 +31,12 @@ namespace Methods
             var result = number1 + number2;
             return result;
 
+        }
+
+        static int Add3(ref int number1,int number2)
+        {
+            number1 = 30;
+            return number1 + number2;
         }
 
     }
