82107ff9daeb11e0c058d191d3faff987a3e7040
sevvalkaba0@gmail.com <sevvalkaba0@gmail.com>
2023-10-24 21:38:54 +0300

    C# Dersleri 25: Out Keyword İle Çalışmak

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -12,9 +12,9 @@ namespace Methods
             //Add();
             //var result = Add2();
 
-            int number1 = 20;
+            int number1;
             int number2 = 100;
-            var result2 = Add3(ref number1, number2);
+            var result2 = Add3(out number1, number2);
             Console.WriteLine(result2);
             Console.WriteLine(number1);
             Console.ReadLine();
@@ -33,7 +33,7 @@ namespace Methods
 
         }
 
-        static int Add3(ref int number1,int number2)
+        static int Add3(out int number1,int number2)
         {
             number1 = 30;
             return number1 + number2;
