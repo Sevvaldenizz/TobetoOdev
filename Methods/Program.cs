f8581d14bee5b35f49e51213a75ed9de7a58c67e
sevvalkaba0@gmail.com <sevvalkaba0@gmail.com>
2023-10-24 21:06:13 +0300

    C# Dersleri 22: Parametreli Metotlarla Çalışmak

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Methods
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace Methods
 {
     internal class Program
     {
@@ -8,12 +10,23 @@
             Add();
             Add();
             Add();
+            var result = Add2(20, 30);
+            Console.WriteLine(result);
             Console.ReadLine();
-          
+
         }
 
         static void Add()
-        {  Console.WriteLine("Added!!!");
-            }
+        {
+            Console.WriteLine("Added!!!");
+        }
+
+        static int Add2(int number1, int number2)
+        {
+            var result = number1 + number2;
+            return result;
+
+        }
+
     }
 }
