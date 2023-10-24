39ec400af2171ec5bbde45f9a3592c0a8568b6a3
sevvalkaba0@gmail.com <sevvalkaba0@gmail.com>
2023-10-24 16:31:45 +0300

    C# Dersleri 5: Integer Veri Tipi

---
+++ TypesAndVariables/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace TypesAndVariables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Value Types
+            // Console.WriteLine("Hello, World!");
+
+
+            int number1 = 2147483647;
+
+
+            Console.WriteLine("Number1 is {0}", number1);
+          
+        }
+    }
+}
